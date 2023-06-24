4c8fedc6a9b195eb72192764cadf1800d6a0ed96
krishpranav <krisna.pranav@gmail.com>
2023-06-24 08:34:44 +0530

    routes: godzilla[DEV]

---
+++ routes/src/main/java/org/godzilla/DEV.java
@@ -0,0 +1,13 @@
+package org.godzilla;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+// @RuntimeMode() // need to be implementded
+public @interface Dev {
+}
