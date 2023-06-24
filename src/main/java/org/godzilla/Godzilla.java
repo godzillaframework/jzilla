29e209a84503016dfb5c44423071b18cbbed3a5d
krishpranav <krisna.pranav@gmail.com>
2023-06-24 10:59:39 +0530

    godzilla: Main -> Godzilla

--- src/main/java/org/godzilla/Godzilla.java
+++ src/main/java/org/godzilla/Godzilla.java
@@ -1,8 +1,10 @@
 package org.godzilla;
 
+import com.sun.net.httpserver.HttpServer;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Godzilla {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
